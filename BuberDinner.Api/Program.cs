0e33c3c95dd73fd40d34bae9e0cbb93464f200a5
Myat Soe <myatsoe.sbs@gmail.com>
2023-06-13 16:58:04 +0630

    first commit

---
+++ BuberDinner.Api/Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+{
+
+builder.Services.AddControllers();
+}
+var app = builder.Build();
+
+
+
+{
+    app.UseHttpsRedirection();
+
+
+app.MapControllers();
+
+app.Run();
+}
